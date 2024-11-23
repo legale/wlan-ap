748970ac2e56f7b686fc857471d4790db415585a
Isaev Ruslan <legale.legale@gmail.com>
2024-11-23 12:15:29 +0300

    ipq50xx: fix fap655 recipe: add factory.ubi image generation
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
+++ feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
@@ -110,11 +110,13 @@ endef
 TARGET_DEVICES += wallys_dr5018
 
 define Device/yuncore_fap655
-  DEVICE_TITLE := Yuncore FAP650
+  DEVICE_TITLE := Yuncore FAP655
   DEVICE_DTS := qcom-ipq5018-yuncore-fap655
   SUPPORTED_DEVICES := yuncore,fap655
   DEVICE_PACKAGES := ath11k-wifi-yuncore-fap655 ath11k-firmware-ipq50xx-map-spruce -kmod-usb-dwc3-of-simple kmod-usb-dwc3-qcom kmod-usb3
   DEVICE_DTS_CONFIG := config@mp03.3
+  IMAGES += factory.ubi
+  IMAGE/factory.ubi := append-ubi
 endef
 TARGET_DEVICES += yuncore_fap655
 
